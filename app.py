da22594191aed75015aaa3eb40bed4db0c3ae250
JulianYZ44 <julian.officiel44@gmail.com>
2025-06-13 20:20:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,16 @@
-from flask import Flask, request, jsonify, send_file
-import os
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
-latest_command = ""
 
-@app.route('/receive_command', methods=['POST'])
-def receive_command():
-    global latest_command
-    latest_command = request.json.get("command", "")
-    return 'OK'
+commands = {}
 
-@app.route('/poll_command')
-def poll_command():
-    global latest_command
-    cmd = latest_command
-    latest_command = ""
-    return jsonify({"command": cmd})
-
-@app.route('/upload_history', methods=['POST'])
-def upload_history():
-    file = request.files['file']
-    file.save("url.txt")
-    return 'Received'
-
-@app.route('/get_url_file')
-def get_url_file():
-    return send_file("url.txt", as_attachment=True)
+@app.route('/', methods=['GET', 'POST'])
+def gateway():
+    if request.method == 'POST':
+        command = request.json.get('command')
+        commands['command'] = command
+        return jsonify({'status': 'success'}), 200
+    return jsonify(commands), 200
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=10000)
-
+    app.run(host='0.0.0.0', port=5000)
